005d21ca90406c7631576f57fd37f65f6521ea44
Joe Esposito <joe@joeyespo.com>
2012-07-31 19:32:02 -0400

    Initial working project.

---
+++ thegitserve.py
@@ -0,0 +1,25 @@
+"""\
+The Git Serve
+
+The Github lottery.
+"""
+
+from flask import Flask
+
+__version__ = '0.1'
+
+
+# Flask application
+app = Flask(__name__)
+app.config.from_object('default_config')
+
+
+#Views
+@app.route('/')
+def index():
+    return 'The GitServe'
+
+
+# Run development server
+if __name__ == '__main__':
+    app.run(app.config['HOST'], app.config['PORT'], app.debug != False)
