d37c3f61d97a3abbcd02f0d8d963f940e8e95e7f
Joe Esposito <joe@joeyespo.com>
2012-07-31 19:38:13 -0400

    Added the default config.

---
+++ default_config.py
@@ -0,0 +1,19 @@
+"""\
+Default Config
+
+Do NOT change the values here.
+Override them in the instance/application_config.py file instead.
+"""
+
+
+# Development settings
+DEBUG = None
+HOST = 'localhost'
+PORT = 5000
+
+# Security settings
+SECRET_KEY = None
+
+# User feedback settings
+ANALYTICS_SCRIPT = None
+FEEDBACK_BLOCK = None
